a5d83ce92c294bfed13ebfa1eb10a2fc3493a7fb
aymen6@gmail.com <aymen6@gmail.com>
2020-11-11 04:33:55 +0100

    adding circtuit breaker to all

---
+++ service-log/CircuitBreaker/circuitBreaker.js
@@ -0,0 +1,25 @@
+const CircuitBreaker = require('opossum');
+
+const response = require('../Handler/HandlerUser/response.controller');
+
+
+const options = {
+  timeout: 3000, 
+  errorThresholdPercentage: 50, // When 50% of requests fail, trip the circuit
+  resetTimeout: 30000 // After 30 seconds, try again.
+};
+
+module.exports.handleBreak =  function handleBreak(callbackFunction){
+    const breaker = new CircuitBreaker(callbackFunction, options);
+
+    breaker.fire()
+      .then((result) => {
+          console.log(result)
+          response.response(result.type,result.res,result.msg,result.status,result.user)
+      })
+      .catch((result) => {
+        response.response(result.type,result.res,result.msg,result.status,result.user)
+    });
+}
+
+
