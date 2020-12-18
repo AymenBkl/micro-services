248ea3b076ff7dbadfc07d751aaa61b7eb0d3d67
Aymen Abdelmoumen Boukhelkhal <a.boukhalkhel@esi-sba.dz>
2020-12-18 23:39:14 +0100

    new search

--- service-products/Controllers/searchProduct.js
+++ service-products/Controllers/searchProduct.js
@@ -1,21 +1,23 @@
 const product = require('../Models/product');
 
+const mainProduct = require('../Models/main-product');
+
 const response = require('../Handler/HandlerProduct/response.controller');
 const category = require('../../service-category/Models/category');
 
-const request = require('../Request/requestCategories').request;
+const request = require('../Request/requestUser').request;
 
 module.exports.searchProduct = (req, res) => {
     console.log(req.body.products);
-    product.find({
-        $or: req.body.products
-    })
+    product.find()
+    .populate({path: 'mainProduct',match : {$or : req.body.products}})
         .then(products => {
+            console.log(products);
             if (products && products.length != 0) {
-                filterProducts(products, req)
-                    .then(products => {
-                        if (products && products.length != 0) {
-                            response.response("success", res, "Products Found", 200, products);
+                sendReqToPharmacies(products, req)
+                    .then(productss => {
+                        if (productss && productss.length != 0) {
+                            response.response("success", res, "Products Found", 200, productss);
                         }
                         else {
                             response.response("error", res, "undefined", 404, null);
@@ -32,80 +34,60 @@ module.exports.searchProduct = (req, res) => {
         .catch(err => {
             response.response("error", res, err, 500, null);
         })
+
+
 }
 
-function filterProducts(products, req) {
-    let size = req.body.products.length;
+
+function sendReqToPharmacies(products, req) {
     return new Promise((resolve, reject) => {
-        validCategories(products, size)
-            .then(categoriesId => {
-                if (categoriesId && Object.keys(categoriesId).length != 0){
-                req.body.categoriesId = Object.keys(categoriesId).map(key => { return { _id: key } });
+        getAllPharmaciesId(products)
+            .then(pharmaciesId => {
+                req.body.pharmaciesId = Object.keys(pharmaciesId).map(key => { return { _id: key } });
                 request(req)
                     .then(result => {
                         if (result && result.status == 200) {
-                            console.log("lol",result.category)
-                            result.category.forEach(categorie => {
-                                let categories = JSON.parse(categorie.pharmacy.categories);
-                                console.log(categories);
-                                categories.map(categorie => {
-                                    Object.keys(categoriesId).map(
-                                        categorieId => {
-                                            console.log(categorie);
-                                            if (categorie.category._id == categorieId) {
-                                                categorie.category.products = ({product : Object.values(categoriesId[categorieId])});
-                                            }
-                                        })
-                                })
-                                categorie.pharmacy.categories = JSON.parse(JSON.stringify(categories));
+                            console.log(result.user);
+                            Object.keys(result.user).map(pharmacie => {
+                                console.log(pharmacie, result.user[pharmacie]);
+                                result.user[pharmacie].pharmacy.products =
+                                    JSON.stringify(Object.values(pharmaciesId[result.user[pharmacie].pharmacy_id])
+                                        .map(product => {
+                                            return { product: product };
+                                        }))
+
                             })
-                            resolve(result.category);
+                            console.log(result.user);
+                            resolve(result.user);
                         }
                         else {
                             reject();
                         }
                     })
                     .catch(err => {
-                        console.log(err);
                         reject(err);
                     });
-                } else {
-                    resolve(false);
-                }
             });
 
     });
-
-
 }
 
-function getAllCategoriesId(products) {
+function getAllPharmaciesId(products) {
     return new Promise((resolve, reject) => {
-        let categoriesId = {};
+        let pharmaciesId = {};
+        console.log(products.length);
         products.forEach(product => {
-            if (categoriesId[product.category]) {
+            if (product.mainProduct != null){
+
+            if (pharmaciesId[product.pharmacy]) {
             }
             else {
-                categoriesId[product.category] = {};
+                pharmaciesId[product.pharmacy] = {};
             }
-            categoriesId[product.category][product._id] = product;
+            pharmaciesId[product.pharmacy][product._id] = product;
+        }
         });
-        resolve(categoriesId);
-    });
-}
-
-function validCategories(products, size) {
-    return new Promise((resolve, reject) => {
-
-    getAllCategoriesId(products)
-        .then((categoriesId) => {
-            Object.keys(categoriesId)
-                .map(category => {
-                    if (Object.keys(categoriesId[category]).length !== size){
-                        delete categoriesId[category];
-                    }
-                })
-            resolve(categoriesId);
-        });
+        console.log(pharmaciesId);
+        resolve(pharmaciesId);
     });
 }
