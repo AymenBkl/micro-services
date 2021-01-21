b6505e7072a08a06a96bf27385a0d442bafcea32
Aymen Abdelmoumen Boukhelkhal <a.boukhalkhel@esi-sba.dz>
2021-01-21 05:08:15 +0100

    comments works

--- service-prescriptions/Controllers/getAllPrescription.js
+++ service-prescriptions/Controllers/getAllPrescription.js
@@ -3,10 +3,16 @@ const response = require('../Handler/HandlerPrescription/response.controller');
 
 const prescription = require('../Models/prescription');
 
+const comment = require('../Models/commet');
+
+const product = require('../Models/product');
+
+const mainProduct = require('../Models/main-product');
 
 module.exports.getAllPrescriptions = (res,req) =>{
     console.log(req.query.id);
     prescription.find({patient: req.query.id})
+    .populate({path:"comments",populate:{path:'products',populate:{path:'mainProduct'}}})
         .then(prescriptions => {
             if (prescriptions){
                 response.response("success",res,"CATEGORY CREATED",200,prescriptions);
---
+++ service-prescriptions/Models/product.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+
+const productSchema = new Schema({
+
+    
+    price : {
+        type : Number,
+        required : true
+    },
+    description : {
+        type : String,
+    },
+
+    mainProduct : {
+        type : mongoose.Types.ObjectId,
+        ref : 'mainproduct',
+        required : true
+    },
+    pharmacy : {
+        type : mongoose.Types.ObjectId,
+        ref : 'User',
+        required : true
+    },
+    imageUrl : {
+        type : String,
+        default : '',
+    },
+} , {
+    timestamps : true,
+}
+)
+
+
+module.exports = mongoose.model('product',productSchema);
