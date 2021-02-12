cebacf83e663ea0dc5a798e231032b67370a7ba2
Aymen Abdelmoumen Boukhelkhal <a.boukhalkhel@esi-sba.dz>
2021-02-12 17:26:51 +0100

    updated

--- service-products/Controllers/addProduct.js
+++ service-products/Controllers/addProduct.js
@@ -2,18 +2,23 @@ const product = require('../Models/product');
 
 const response = require('../Handler/HandlerProduct/response.controller');
 
-module.exports.addProduct = (res,body) =>{
-    console.log(body);
-    product.create(body)
+module.exports.addProduct = (res, body) => {
+        var query = {mainProduct:body.mainProduct},
+         update = { status: 'active',price: body.price,description:body.description},
+         options = { upsert: true, new: true, setDefaultsOnInsert: true };
+         console.log(query);
+         console.log(options);
+         console.log(update);
+    product.findOneAndUpdate(query,update,options)
         .then(product => {
-            if (product){
-                response.response("success",res,"Product CREATED",200,product);
+            if (product) {
+                response.response("success", res, "Product CREATED", 200, product);
             }
             else {
-                response.response("error",res,"undefined",404,null);
+                response.response("error", res, "undefined", 404, null);
             }
-        } )
+        })
         .catch(err => {
-            response.response("error",res,err,500,null);
+            response.response("error", res, err, 500, null);
         })
 }
